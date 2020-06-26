e4a1fc744e1b38c8f0cdee1d61d0a97ff140ae7c
sergio fedoroff <fiodorovs@yahoo.com>
2020-06-25 23:36:04 -0500

    leccion 10 completa

--- Leccion10/fechas.js
+++ Leccion10/fechas.js
@@ -1,38 +1,65 @@
-/*Este programa calcula el cumpleaños de una persona*/
-
+/*Este programa calcula cuantos años tiene una persona
+ y chequea si hay cumpleaños el mes corriente*/
 
 var estudiantes = {
 
-    "0001": new Date(2003, 1, 22),
-    "0002": new Date(2002, 9, 25),
-    "0003": new Date(2003, 4, 9),
-    "0004": new Date(2001, 3, 20),
-    "0005": new Date(2002, 4, 15),
-    "0006": new Date(2003, 5, 6),
-    "0007": new Date(2002, 11, 3),
-    "0008": new Date(2003, 8, 11),
-    "0009": new Date(2001, 4, 9 ),
-    "1000": new Date(2004, 3, 20) 
+    "Juan": new Date(2003, 1, 22),
+    "Adrian": new Date(2002, 9, 25),
+    "Carolina": new Date(2003, 4, 9),
+    "Alberto": new Date(2001, 3, 20),
+    "Roberto": new Date(2002, 4, 15),
+    "Alejandro": new Date(2003, 4, 30),
+    "Luna": new Date(2002, 11, 3),
+    "Maria": new Date(2003, 8, 11),
+    "Carlos": new Date(2001, 4, 9 ),
+    "Jasmin": new Date(2004, 4, 25) 
 };
-
 
 
 function calculaEdad(fecha){
 
     let fechaDeHoy = new Date();
-    let fechaNacimiento = new Date (fecha);
+    let fechaNacimiento = new Date(fecha);
     let edadCalculada = fechaDeHoy.getFullYear() - fechaNacimiento.getFullYear();
     let diferenciaMes = fechaDeHoy.getMonth() - fechaNacimiento.getMonth();
+    
+    //reducimos la edad por un año si es que no pasó el día del cumpleaños.
 
-    if ((diferenciaMes < 0) || ( diferenciaMes === 0)&&(fechaDeHoy.getDate()< fechaNacimiento.getDate())){
+    if ((diferenciaMes < 0) || ( diferenciaMes === 0) && (fechaDeHoy.getDate() < fechaNacimiento.getDate())){
         edadCalculada = edadCalculada - 1;
     }
 
     return edadCalculada;
 }
 
+//Imprimimos cuantos años tiene cada estudiante usando una plantilla de cadena de texto
+
 for (persona in estudiantes) {
-    console.log( persona + " tiene " + calculaEdad(estudiantes[persona]) + " años");
+    console.log(` ${persona} tiene ${calculaEdad(estudiantes[persona])} años.`);
 }
 
+//Chequea si hay un cumpleaños en el mes corriente
 
+function chequeaCumpleanos(){
+ var fechaHoy = new Date();
+ for(persona in estudiantes) {
+    
+    var x = fechaHoy.getMonth() - estudiantes[persona].getMonth();
+    var y = estudiantes[persona].getDate() - fechaHoy.getDate();
+    var numCumpleanos = 0;
+
+    if(x == 0 && y > 0) {
+    console.log(`faltan ${y} dias para el cumpleaños de ${persona}`); 
+    numCumpleanos++;
+    }
+    if (x == 0 && y == 0){
+         console.log (`!Atención! Hoy es el cumpleaños de ${persona}`);
+         numCumpleanos++;
+        }
+  }
+ if (numCumpleanos == 0){
+      console.log ("No hay cumpleaños este mes.");
+  }  
+}
+
+chequeaCumpleanos();
